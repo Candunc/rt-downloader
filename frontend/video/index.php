323e1b082a65fdb4ccf6d7008b4f1e2efd623c61
Duncan Bristow <duncbristow@msn.com>
2016-11-04 14:18:15 -0600

    Added a fail condition for the video page

--- frontend/video/index.php
+++ frontend/video/index.php
@@ -5,7 +5,11 @@ $hash = 'd56ee6a6e5f7bed12cac96b4ab89bf0730aa4f624432b272c20b2e509c69d2c0';
 
 $db = new SQLite3('rt.sqlite3');
 
-$table = SQLite3::escapeString($db->querySingle('SELECT site FROM Metadata WHERE hash IS "' . $hash . '"')); #Because we keep all the data in a site-specific table, we have to lookup the site...
-$results = $db->querySingle('SELECT * FROM ' . $table . ' WHERE hash IS "' . $hash . '"', true);
+$table = $db->querySingle('SELECT site FROM Metadata WHERE hash IS "' . $hash . '"'); #Because we keep all the data in a site-specific table, we have to lookup the site...
+if (!$table) {
+	echo('404 error goes here.');
+	die();
+}
+$results = $db->querySingle('SELECT * FROM ' . $table . ' WHERE hash IS "' . SQLite3::escapeString($hash) . '"', true); #Be careful when using the description. If you ECHO it straight we have the potential for XSS.
 var_dump($results);
 ?>
