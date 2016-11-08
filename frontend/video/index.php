9f4403cbf09350ba5797a636df1a0a141211f7b6
Duncan Bristow <duncbristow@msn.com>
2016-11-08 12:12:53 -0700

    Added css to force 16:9 aspect ratio. This is because Fan Service has a 3:2 image, which of course, breaks everything.

--- frontend/video/index.php
+++ frontend/video/index.php
@@ -1,15 +1,20 @@
 <?php #Todo: Create the html template for this page.
+function invalid() {
+	http_response_code(400);
+	echo('400 Bad Request');
+	die();
+}
+
+if (!$_GET['v']) { invalid(); }
+
 #$hash = SQLite3::escapeString($_GET['v']);
 $hash = 'd56ee6a6e5f7bed12cac96b4ab89bf0730aa4f624432b272c20b2e509c69d2c0';
-#We should check if the hash exists, and if not redirect to 404 page.
 
 $db = new SQLite3('rt.sqlite3');
 
 $table = $db->querySingle('SELECT site FROM Metadata WHERE hash IS "' . $hash . '"'); #Because we keep all the data in a site-specific table, we have to lookup the site...
-if (!$table) {
-	echo('404 error goes here.');
-	die();
-}
+if (!$table) { invalid(); }
+
 $results = $db->querySingle('SELECT * FROM ' . $table . ' WHERE hash IS "' . SQLite3::escapeString($hash) . '"', true); #Be careful when using the description. If you ECHO it straight we have the potential for XSS.
 var_dump($results);
 ?>
