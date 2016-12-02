3a7f1d7d39ba3380a07ff2c576c7ec8013d9a4d8
Duncan Bristow <duncbristow@msn.com>
2016-12-02 07:45:10 -0700

    Some old uncommitted changes.

--- node/config.example.lua
+++ node/config.example.lua
@@ -1,4 +1,7 @@
 config = {
+	--The full filepath pointing to the root of the domain. IE /var/www/rtdownloader
+	www_dir = ""
+
 --	this corresponds to whatever url is used to access the local webserver.
 	local_url = "https://example.com";
 
--- node/main.lua
+++ node/main.lua
@@ -89,8 +89,14 @@ log("Downloading video '"..input["title"].."'")
 
 exec("/usr/local/bin/youtube-dl -u \""..config["username"].."\" -p \""..config["password"].."\" -o \""..input["hash"].."_temp.mp4\" \"https://"..input["channelUrl"].."/episode/"..input["slug"].."\"")
 
---From http://superuser.com/a/522853/607043, need to look more into optimization.
-exec("ffmpeg -i \""..input["hash"].."_temp.mp4\" -c:v libx264 -crf 18 -preset medium  -c:a copy \""..input["hash"]..".mp4\"")
+if input["showName"] == "RT Animated Adventures" then
+	--"Custom" optimization. Because of the video's style, it can be compressed much more.
+	exec("ffmpeg -i \""..input["hash"].."_temp.mp4\" -c:v libx264 -crf 18 -preset slow  -c:a copy \""..input["hash"]..".mp4\"")
+else
+	--From http://superuser.com/a/522853/607043, need to look more into optimization.
+	exec("ffmpeg -i \""..input["hash"].."_temp.mp4\" -c:v libx264 -crf 22 -preset medium  -c:a copy \""..input["hash"]..".mp4\"")
+end
+
 exec("rm \""..input["hash"]..".mp4\"")
 
 --Now here is where we report the completed video to the frontend.
